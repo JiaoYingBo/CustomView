658aec9b5da80bde394feeee25da6a687757294b
JYB <715775799@qq.com>
2016-08-18 15:49:03 +0800

    Initial Commit

---
+++ CustomView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomView
+//
+//  Created by Mac on 16/8/18.
+//  Copyright © 2016年 JYB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomView
+//
+//  Created by Mac on 16/8/18.
+//  Copyright © 2016年 JYB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
